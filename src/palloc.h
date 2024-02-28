5af187b0320893004771fd7ee8eeae049df9c3f6
Yersa Nordman <yersa@finwo.nl>
2024-02-28 23:40:51 +0100

    Added file structure back to the readme

--- src/palloc.h
+++ src/palloc.h
@@ -166,3 +166,27 @@ uint64_t palloc_next(struct palloc_t *pt, uint64_t ptr);
 #ifdef __cplusplus
 } // extern "C"
 #endif
+
+/// File structure
+/// --------------
+///
+/// - header
+///     - 4B header "PBA\0"
+///     - uint16_t  flags
+/// - blobs */
+///     - 8B free + size
+/// - size indicator: data only, excludes size indicator itself
+/// - free flag:
+///     - 1 = free
+///     - 0 = occupied
+/// - blob structure:
+///     - free:
+///         - 8B size | flag
+///         - 8B pointer previous free block (0 = no previous free block)
+///         - 8B pointer next free block (0 = no next free block)
+///         - 8B size | flag
+///     - occupied:
+///         - 8B size
+///         - &lt;data[n]&gt;
+///         - 8B size
+
