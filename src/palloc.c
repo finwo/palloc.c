ba2ff08b1d4c9406acccd84ce968d77b4058e8cf
Yersa Nordman <finwo@pm.me>
2024-05-11 22:37:14 +0200

    Using finwo/io.h lib for handling os-specific io methods

--- src/palloc.c
+++ src/palloc.c
@@ -27,6 +27,7 @@ extern "C" {
 #endif
 
 #include "finwo/canonical-path.h"
+#include "finwo/io.h"
 
 #include "palloc.h"
 
@@ -48,40 +49,13 @@ extern "C" {
 
 #define PALLOC_MARKER_FREE (0x8000000000000000)
 
-// OS-specific IO macros {{{
 #if defined(_WIN32) || defined(_WIN64)
-#define stat_os __stat64
-#define fstat_os _fstat64
-#define seek_os _lseeki64
-#define open_os _open
-#define write_os _write
-#define read_os _read
-#define close_os _close
-#define O_CREAT _O_CREAT
-#define O_RDWR  _O_RDWR
 #define OPENMODE  (_S_IREAD | _S_IWRITE)
-#define O_DSYNC 0
-#define ssize_t SSIZE_T
 #elif defined(__APPLE__)
-#define stat_os stat
-#define fstat_os fstat
-#define seek_os lseek
-#define open_os open
-#define write_os write
-#define read_os read
-#define close_os close
 #define OPENMODE  (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
 #else
-#define stat_os stat64
-#define fstat_os fstat64
-#define seek_os lseek64
-#define open_os open
-#define write_os write
-#define read_os read
-#define close_os close
 #define OPENMODE  (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
 #endif
-// }}}
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
--- test.c
+++ test.c
@@ -20,43 +20,15 @@ extern "C" {
 #include <unistd.h>
 #endif
 
-// OS-specific IO macros {{{
+#include "finwo/io.h"
+
 #if defined(_WIN32) || defined(_WIN64)
-#define stat_os __stat64
-#define fstat_os _fstat64
-#define seek_os _lseeki64
-#define open_os _open
-#define write_os _write
-#define read_os _read
-#define close_os _close
-#define unlink_os _unlink
-#define O_CREAT _O_CREAT
-#define O_RDWR  _O_RDWR
 #define OPENMODE  (_S_IREAD | _S_IWRITE)
-#define O_DSYNC 0
-#define ssize_t SSIZE_T
 #elif defined(__APPLE__)
-#define stat_os stat
-#define fstat_os fstat
-#define seek_os lseek
-#define open_os open
-#define write_os write
-#define read_os read
-#define close_os close
-#define unlink_os unlink
 #define OPENMODE  (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
 #else
-#define stat_os stat64
-#define fstat_os fstat64
-#define seek_os lseek64
-#define open_os open
-#define write_os write
-#define read_os read
-#define close_os close
-#define unlink_os unlink
 #define OPENMODE  (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP)
 #endif
-// }}}
 
 void test_init() {
   char *testfile = "pizza.db";
